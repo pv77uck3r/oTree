8f7289d2372d54c71720c9a25f25f0995f63b4ee
pv77uck3r <jralston@uci.edu>
2018-09-18 14:00:24 -0500

    Turning Debug Off

--- settings.py
+++ settings.py
@@ -93,7 +93,7 @@ ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 
 
 # Consider '', None, and '0' to be empty/false
-DEBUG = True
+DEBUG = False
 
 DEMO_PAGE_INTRO_HTML = """
 Here are various games implemented with 
